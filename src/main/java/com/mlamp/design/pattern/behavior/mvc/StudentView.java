3fee7b9000ec41e22a61f36fc8828610490b9e2f
liuxiaohu <liuxiaohu@mininglamp.com>
2018-09-30 11:56:49 +0800

    add the mvc pattern implementation demo
    We are going to create a Student object acting as a model.StudentView
    will be a view class which can print student details on console and
    StudentController is the controller class responsible to store data in
    Student object and update view StudentView accordingly.
    
    MVCPatternDemo, our demo class, will use StudentController to
    demonstrate use of MVC pattern.

---
+++ src/main/java/com/mlamp/design/pattern/behavior/mvc/StudentView.java
@@ -0,0 +1,12 @@
+package com.mlamp.design.pattern.behavior.mvc;
+
+/**
+ * create view layer
+ */
+public class StudentView {
+    public void printStudentDetails(String studentName, String studentRollNo){
+        System.out.println("Student: ");
+        System.out.println("Name: " + studentName);
+        System.out.println("Roll No: " + studentRollNo);
+    }
+}
